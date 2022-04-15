18f8d97dd76a7babfeabab82a87d68db6b2d7f15
dopebnan <82271322+dopebnan@users.noreply.github.com>
2022-04-15 17:06:14 +0300

    Custom errors

---
+++ bot/errors.py
@@ -0,0 +1,47 @@
+from discord.errors import DiscordException
+
+__all__ = (
+    "GenericError",
+    "AuthorNotInVoice",
+    "NoVoiceClient",
+    "BadArgument",
+    "BadAttachment",
+    "NoAttachment",
+    "EmptyQueue"
+)
+
+
+class GenericError(DiscordException):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class VoiceClientError(GenericError):
+    pass
+
+
+class AuthorNotInVoice(VoiceClientError):
+    def __init__(self, message="Author not in any voice channel"):
+        super().__init__(message)
+
+
+class NoVoiceClient(VoiceClientError):
+    def __init__(self, message="Bot isn't in any voice channel"):
+        super().__init__(message)
+
+class EmptyQueue(VoiceClientError):
+    pass
+
+class BadArgument(GenericError):
+    def __init__(self, message, arg):
+        self.message = message
+        super().__init__(f"'{arg}' -> {message}")
+
+
+class BadAttachment(BadArgument):
+    pass
+
+
+class NoAttachment(GenericError):
+    pass
